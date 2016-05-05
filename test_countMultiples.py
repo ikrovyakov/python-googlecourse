ce06b47ec242e1d9ef6a29f0cae8cdaec9702451
ikrovyakov <ikrovyakov@yandex.ru>
2016-05-06 02:08:39 +0300

    proper logging added, still getting errors working on unit tests

---
+++ test_countMultiples.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+from testrun import countMultiples
+
+
+class TestCountMultiples(TestCase):
+    # def test_countMultiples(self):
+    #     self.fail()
+
+    def test1(self):
+        self.assertEqual(countMultiples([3, 5], 100), 46)
+
+    def test2(self):
+        self.assertEqual(countMultiples([3, 5, 7], 30), 16)
+
+    def test3(self):
+        self.assertEqual(countMultiples([3], 30), 9)
+
+    def test4(self):
+        self.assertEqual(countMultiples([13, 25], 100250), 11412)
+
+    def test5(self):
+        self.assertEqual(countMultiples([13, 35, 47], 389560), 47673)
+
+    def test6(self):
+        self.assertEqual(countMultiples([3], 389750), 129916)
+
+if __name__ == '__main__':
+
+    unittest.main()
--- testrun.py
+++ testrun.py
@@ -1,4 +1,5 @@
 # import sys
+import logging
 
 
 def countMultiples(l, max):
@@ -6,18 +7,23 @@ def countMultiples(l, max):
     return the number of multiples of each of those numbers that are less than the maximum number.
     For this case the list will contain a maximum of 3 numbers that are all relatively prime to each other."""
     num = 0
-    for i in range(0,len(l)):
+    logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger("countMultiples")
+    logger.info('countMultiples started with following parameters: {0}, {1}'.format(l, max))
+    for i in range(0, len(l)):
         num += (max-1) // l[i]
-        print '%d of %d in %d' % ((max-1) // l[i],l[i],max)
-        if i>0:
-            for j in range(0,i):
-                num -= (max-1) // (l[i]*l[j])
-                print 'Subtracting %d of %d * %d in %d' % ((max-1) // (l[i]*l[j]), l[i], l[j], max)
+        logger.info('    {0} of {1} in {2}'.format((max-1) // l[i], l[i], max))
+        if i > 0:
+            for j in range(0, i):
+                num -= (max-1) // (l[i] * l[j])
+                logger.info('    Subtracting {0} of {1} * {2} in {3}'.format((max-1) // (l[i] * l[j]), l[i], l[j], max))
+    logger.info('countMultiples result: {0}'.format(num))
     return num
 
 
 def main():
-    print countMultiples([13, 35, 47], 389560)
+    countMultiples([13, 35, 47], 389560)
     return
 
 
